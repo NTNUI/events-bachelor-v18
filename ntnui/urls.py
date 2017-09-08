58b615823677177620d8892f452e12bae3fbbd8b
Thor Even Tutturen <thor@bustbyte.no>
2017-09-08 09:44:25 +0200

    add dummy index route

--- ntnui/urls.py
+++ ntnui/urls.py
@@ -13,9 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+
+    # temporary route, just to have something at index
+    url(r'^$', include('groups.urls')),
+    url(r'^groups/', include('groups.urls'))
+
 ]
