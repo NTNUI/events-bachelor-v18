b0a18c7fe262bdba9f2a3fdbbcb7c1b2e0577f73
Thor Even Tutturen <thor@bustbyte.no>
2017-09-11 09:40:41 +0200

    simplify login

--- ntnui/urls.py
+++ ntnui/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -22,5 +23,7 @@ urlpatterns = [
     # temporary route, just to have something at index
     url(r'^', include('groups.urls')),
     url(r'^groups/', include('groups.urls')),
-    url(r'^accounts/', include('django.contrib.auth.urls'))
+    url(r'^logout/', auth_views.logout, name='logout'),
+    url(r'^login/', auth_views.login, name='login'),
+    url(r'^login/', auth_views.login, name='login'),
 ]
