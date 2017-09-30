4cf100f42f29528f0768d28e5777023d540fb420
Thor Even Tutturen <thor@bustbyte.no>
2017-09-30 12:44:20 +0200

    add fake users in view function

--- groups/views.py
+++ groups/views.py
@@ -4,7 +4,102 @@ from django.contrib.auth.decorators import login_required
 
 @login_required
 def members(request):
-    return render(request, 'groups/members.html')
+    group = {
+        'name': 'NTNUI Volleyball',
+        'members': [
+            {
+                'name': 'Todd Packer',
+                'phone': '93827492',
+                'email': 'todd.packer@online.com',
+                'paid': False,
+            },
+            {
+                'name': 'James Halpert',
+                'phone': '23092340',
+                'email': 'jameshalpert@gmail.com',
+                'paid': True,
+            },
+            {
+                'name': 'Angela Frankenstein',
+                'phone': '21093801',
+                'email': 'frankela@steinberg.org',
+                'paid': True,
+            },
+            {
+                'name': 'Michael Scott',
+                'phone': '99883212',
+                'email': 'michael.scott@dundermifflin.com',
+                'paid': False,
+            },
+            {
+                'name': 'Creed Bratton',
+                'phone': '49289201',
+                'email': 'mynameiscreed@bratton.com',
+                'paid': False,
+            },
+            {
+                'name': 'Phyllis Vance',
+                'phone': '99883212',
+                'email': 'phyllis@vancerefrigiration.com',
+                'paid': True,
+            },
+            {
+                'name': 'Stanley Hudson',
+                'phone': '49209281',
+                'email': 'stanley.hudson@dundermifflin.com',
+                'paid': True,
+            },
+            {
+                'name': 'Dwight K. Schrute',
+                'phone': '49209281',
+                'email': 'dwight.k.schrute@schrutefarms.net',
+                'paid': True,
+            },
+            {
+                'name': 'Toby Flenderson',
+                'phone': '93282311',
+                'email': 'toby.flenderson@dundermifflin.com',
+                'paid': True,
+            },
+            {
+                'name': 'Pam Beesly',
+                'phone': '44920082',
+                'email': 'beeslyarts@gmail.com',
+                'paid': False,
+            },
+            {
+                'name': 'Ryan Howard',
+                'phone': '94827328',
+                'email': 'ryan.the.fireguy@hotmail.com',
+                'paid': False,
+            },
+            {
+                'name': 'Andrew Bernard',
+                'phone': '49209281',
+                'email': 'andrew.bernard@cornell.edu',
+                'paid': True,
+            },
+            {
+                'name': 'Robert California',
+                'phone': '99382392',
+                'email': 'givemeonemillion@robertcalifornia.com',
+                'paid': False,
+            },
+            {
+                'name': 'Jan Levinson',
+                'phone': '11839238',
+                'email': 'jan.levinson@dundermifflin.com',
+                'paid': True,
+            },
+            {
+                'name': 'Roy Anderson',
+                'phone': '88292038',
+                'email': 'thebestroy@aol.com',
+                'paid': False,
+            },
+        ]
+    }
+    return render(request, 'groups/members.html', { 'group': group })
 
 
 def list_groups(request):
