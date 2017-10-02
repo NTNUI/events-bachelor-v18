2fbc570c2904a333f73b6e88397d474c7cf611b5
Anders Klever Kirkeby <anders@klever-kirkeby.com>
2017-10-02 16:11:34 +0200

    add settings view, along with tests for both view and form submission

--- groups/urls.py
+++ groups/urls.py
@@ -1,5 +1,4 @@
 from django.conf.urls import url
-from django.views.generic.base import RedirectView
 from . import views
 
 urlpatterns = [
@@ -7,4 +6,5 @@ urlpatterns = [
     url(r'^(?P<slug>[\w-]+)/members$', views.members, name='group_members'),
     url(r'^(?P<slug>[\w-]+)/members/invite$', views.invite_member, name='group_invite_member'),
     url(r'^(?P<slug>[\w-]+)$', views.group_index, name='group_index'),
+    url(r'^(?P<slug>[\w-]+)/settings$', views.settings, name='group_settings'),
 ]
