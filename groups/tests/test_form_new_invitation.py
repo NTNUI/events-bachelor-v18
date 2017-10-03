e46d56ff9da588734bc429d9f596d46bba6ff21b
Thor Even Tutturen <thor@bustbyte.no>
2017-10-03 10:06:44 +0200

    add invitations view, more tests for members

--- groups/tests/test_form_new_invitation.py
+++ groups/tests/test_form_new_invitation.py
@@ -6,7 +6,8 @@ from ..models import SportsGroup, Invitation
 
 
 class InvitationFormTest(TestCase):
-    fixtures = ['users.json', 'groups.json', 'memberships.json', 'invitations.json']
+    fixtures = ['users.json', 'groups.json',
+                'memberships.json', 'invitations.json']
 
     def test_form_has_right_fields(self):
         form = NewInvitationForm(slug='volleyball')
@@ -27,24 +28,27 @@ class InvitationFormTest(TestCase):
         form = NewInvitationForm(
             data={'email': 'random-email@random.com'}, slug='volleyball')
         self.assertFalse(form.is_valid())
-        self.assertEqual(form.errors['email'], ['This email address is not connected to a user.'])
+        self.assertEqual(form.errors['email'], [
+                         'This email address is not connected to a user.'])
 
     def test_should_throw_if_email_is_already_member(self):
         form = NewInvitationForm(
             data={'email': 'todd.packer@online.com'}, slug='volleyball')
         self.assertFalse(form.is_valid())
-        self.assertEqual(form.errors['email'], ['This user is already a member of this group.'])
+        self.assertEqual(form.errors['email'], [
+                         'This user is already a member of this group.'])
 
     def test_should_throw_if_email_is_already_invited(self):
         # We know that ryan.the.fireguy@hotmail.com is already invited
         form = NewInvitationForm(
             data={'email': 'ryan.the.fireguy@hotmail.com'}, slug='volleyball')
         self.assertFalse(form.is_valid())
-        self.assertEqual(form.errors['email'], ['This user is already invited.'])
+        self.assertEqual(form.errors['email'], [
+                         'This user is already invited.'])
 
     def test_should_create_a_new_invitation_on_save(self):
         form = NewInvitationForm(
-            data={ 'email': 'meredith.palmer@dundermifflin.com'}, slug='volleyball'
+            data={'email': 'meredith.palmer@dundermifflin.com'}, slug='volleyball'
         )
         self.assertTrue(form.is_valid())
         invitation = form.save()
--- groups/views.py
+++ groups/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse, Http404
 from django.contrib.auth.decorators import login_required
 from .models import SportsGroup, Membership, Invitation
@@ -12,30 +12,31 @@ def group_index(request, slug):
 
 @login_required
 def members(request, slug):
-    groups = SportsGroup.objects.filter(slug=slug)
-    if (len(groups) != 1):
-        raise Http404("Group does not exist")
-    group = groups[0]
+    group = get_object_or_404(SportsGroup, slug=slug)
+    invitations = Invitation.objects.filter(group=group.pk)
     members = Membership.objects.filter(group=group.pk)
     return render(request, 'groups/members.html', {
         'group': group,
         'members': members,
         'total_members': len(members),
+        'total_invitations': len(invitations),
         'slug': slug,
+        'active_tab': 'members',
+
     })
 
 @login_required
 def invitations(request, slug):
-    groups = SportsGroup.objects.filter(slug=slug)
-    if (len(groups) != 1):
-        raise Http404("Group does not exist")
-    group = groups[0]
+    group = get_object_or_404(SportsGroup, slug=slug)
     invitations = Invitation.objects.filter(group=group.pk)
-    return render(request, 'groups/members.html', {
+    members = Membership.objects.filter(group=group.pk)
+    return render(request, 'groups/invitations.html', {
         'group': group,
-        'members': invitations,
+        'invitations': invitations,
         'total_invitations': len(invitations),
+        'total_members': len(members),
         'slug': slug,
+        'active_tab': 'invitations',
     })
 
 
