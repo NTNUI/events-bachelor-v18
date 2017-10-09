21f28a0ce117d3f143f5bfda45e2713ca9898a5e
root <askjent@gmail.com>
2017-10-09 12:14:35 +0200

    Add forms link from groups page and forms html base

--- forms/views.py
+++ forms/views.py
@@ -4,9 +4,9 @@ from django.contrib.auth.decorators import login_required
 
 @login_required
 def list(request, slug):
-    return render(request, 'forms/forms_list.html', {
+    """return render(request, 'forms/forms_list.html', {
         #'group': group,
         'slug': slug,
         'active': 'forms_list',
-    })
-    #return HttpResponse('Hello World')
+    })"""
+    return HttpResponse('Hello World')
