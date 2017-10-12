d06b4a328ff7ec2ffbaa4026f20fec67338f4e67
Erlend M <erlend.morthen@gmail.com>
2017-10-12 15:26:31 +0200

    rename list_all_members.html

--- hs/views.py
+++ hs/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render, redirect, get_object_or_404
 from accounts.models import User
 
 def list_all_members(request):
-    return render(request,'hs/list_all_members.html')
+    return render(request, 'hs/hs_space.html')
 
