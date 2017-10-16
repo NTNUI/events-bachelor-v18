dc2982938555896beeabef56bd030a9773201860
Erlend M <erlend.morthen@gmail.com>
2017-10-16 12:52:54 +0200

    fill list of all users, without group and role, centered text

--- hs/views.py
+++ hs/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from accounts.models import User
 
+
 def hs_space(request):
     return render(request, 'hs/hs_space.html')
 
+
 def list_all_members(request):
     return render(request, 'hs/all_members.html', {
+        'users': User.objects.all(),
         'active': 'all_members',
     })
