10e78dc231a801fff9da7afd05c97b3bff9505b9
Thor Even Tutturen <thor@bustbyte.no>
2017-10-16 12:39:56 +0200

    add boards to selenium tests

--- ntnui/tests/browser/lib/helpers.py
+++ ntnui/tests/browser/lib/helpers.py
@@ -1,11 +1,11 @@
 from django.conf import settings
 
 
-def login_user(cls, browser):
+def login_user(cls, browser, username = settings.DUMMY_USER_EMAIL, password = settings.DUMMY_USER_PASSWORD):
     browser.get(cls.server_url + '/login/')
     username_input = browser.find_element_by_name('username')
-    username_input.send_keys(settings.DUMMY_USER_EMAIL)
+    username_input.send_keys(username)
     password_input = browser.find_element_by_name('password')
-    password_input.send_keys(settings.DUMMY_USER_PASSWORD)
+    password_input.send_keys(password)
     browser.find_element_by_xpath('//button[text()="Log in"]').click()
     account_p = browser.find_element_by_xpath('//p[text()="Account"]')
