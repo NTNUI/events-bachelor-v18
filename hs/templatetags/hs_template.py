3656a938f08d3641c1b2d71079ea4e1c578b85d6
Erlend M <erlend.morthen@gmail.com>
2017-10-25 16:23:58 +0200

    experimenting

--- hs/templatetags/hs_template.py
+++ hs/templatetags/hs_template.py
@@ -7,8 +7,25 @@ register = template.Library()
 def member(value, arg):
     html = ''
     groups = []
+
     for group in value.get(arg):
         html += '<div class="group-member-name">' + group + '</div>'
         groups.append(group)
+
+    return html
+
+
+@register.filter
+def role(value, arg):
+    html = ''
+    print("#################")
+    print("#################")
+    print(arg)
+    print("#################")
+    print(value)
+
+    #for group in value.get(arg):
+     #   for role in group:
+      #      html += '<div class="group-member-role">' + role + '</div>'
 
     return html
