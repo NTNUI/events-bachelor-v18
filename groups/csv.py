adaa6f4ff2d5fb58d0e21c9a615922178bb899cc
Erlend M <erlend.morthen@gmail.com>
2017-11-01 09:22:16 +0100

    cleanup

--- groups/csv.py
+++ groups/csv.py
@@ -18,12 +18,14 @@ def download_all_members(request,slug):
     # Generate a sequence of rows. The range is based on the maximum number of
     # rows that can be handled by a single sheet in most spreadsheet
     # applications.
-    group=get_object_or_404(SportsGroup, slug=slug)
+    # rows = (["Row {}".format(idx), str(idx)] for idx in range(65536))
+    group = get_object_or_404(SportsGroup, slug=slug)
     members = Membership.objects.filter(group=group.pk)
     pseudo_buffer = Echo()
     writer = csv.writer(pseudo_buffer)
     response = StreamingHttpResponse((
-        writer.writerow([member.person.first_name + " " +member.person.last_name, member.person.email,
+        #  writer.writerow(['Name','Email','Phone','Paid fee']),
+        writer.writerow([member.person.first_name + " " + member.person.last_name, member.person.email,
                          member.person.phone]) for member in members),
                                      content_type="text/csv")
     response['Content-Disposition'] = 'attachment; filename=""' + slug + '"members.csv"'
