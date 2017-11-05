72e621646a8b92d6bcec198408314b900d476ed1
Anders Klever Kirkeby <anders@klever-kirkeby.com>
2017-11-05 12:14:40 +0100

    Add forms view submitted page and sub-pages

--- forms/views.py
+++ forms/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 
 from accounts.models import User
-from groups.models import SportsGroup
+from groups.models import SportsGroup, Board
 from django.http import JsonResponse
 from .forms import BoardChangeForm
 from .models import FormDoc, BoardChange
@@ -28,6 +28,7 @@ def fill_form(request, slug):
 
     if request.method == 'POST':
         # create a form instance and populate it with data from the request:
+        print(request.POST)
         form = BoardChangeForm(request.POST, slug=slug)
 
         # check whether it's valid:
@@ -47,12 +48,48 @@ def fill_form(request, slug):
         'form': form
     })
 
+@login_required
+def forms_list_submitted(request):
+    user = request.user
+    forms = []
+    approved = []
+
+    for f in BoardChange.objects.all():
+        if user in f:
+            if user in f.needs_approval_from():
+                forms.append(f)
+            elif len(f.needs_approval_from()) > 0:
+                approved.append(f)
+
+    return render(request, 'forms/forms_list_submitted.html', {
+        'approved' : approved,
+        'forms': forms
+    })
+
+@login_required
+def forms_read(request, forms_id):
+    try:
+        form = BoardChange.objects.get(pk=forms_id)
+        board = Board.objects.get(sports_group=form.group)
+    except BoardChange.DoesNotExist:
+        return redirect('404')
+
+    if request.method == 'POST':
+        if request.POST.get("button") == "approve":
+            form.approve(request.user)
+
+        return redirect('forms_list_submitted')
+
+    return render(request, 'forms/forms_read.html', {
+        'board' : board,
+        'form': form
+    })
+
 #
 # AJAX Spesific Methods
 #
-
 @login_required
-def validate_email(request):
+def ajax_validate_email(request):
     if request.method == "GET":
         email = request.GET.get('email', None)
         id = request.GET.get('id', None)
@@ -74,7 +111,7 @@ def validate_email(request):
 
 
 @login_required
-def group_name(request):
+def ajax_group_name(request):
     if request.method == "GET":
         slug = request.GET.get('group', None)
 
