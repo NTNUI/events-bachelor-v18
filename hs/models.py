ec91cf5fd2720de80e2b0b5adecb3be6c61387aa
Rasmus Stene <rasmunis@gmail.com>
2017-11-06 13:53:02 +0100

    init feat/hs-tab

--- hs/models.py
+++ hs/models.py
@@ -1,3 +1,24 @@
-#from django.db import models
+from django.db import models
+from django.conf import settings
+import os
+from groups.models import Membership, Invitation
+import datetime
 
-# Create your models here.
+
+def get_cover_upload_to(instance, filename):
+    return os.path.join(
+        "cover_photo/%s" % instance.slug, filename)
+
+
+class MainBoard(models.Model):
+    name = models.CharField(max_length=50)
+    slug = models.CharField(max_length=12)
+    cover_photo = models.ImageField(upload_to=get_cover_upload_to, default='cover_photo/ntnui-volleyball.png')
+
+
+class HSMembership(models.Model):
+    person = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    board = models.ForeignKey(MainBoard)
+    date_joined = models.DateField(default=datetime.date.today)
+    role = models.CharField(max_length=30)
