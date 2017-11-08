481b2ca78a7a6ab3f9bc1c1b110e1dfd39e83853
Ask <askjent@gmail.com>
2017-11-08 15:30:33 +0100

    Fix css spacing in forms list

--- forms/models.py
+++ forms/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class FormDoc(models.Model):
     name = models.CharField(max_length=50)
-    description = models.TextField(max_length=200)
+    description = models.TextField(max_length=150)
 
     def __str__(self):
         return self.name
