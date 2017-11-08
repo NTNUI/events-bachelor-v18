4931b9ed66107bec552b27769ef84239a4075545
Anders Klever Kirkeby <anders@klever-kirkeby.com>
2017-11-08 15:42:54 +0100

    Add decorators for is_main_board

--- ntnui/decorators.py
+++ ntnui/decorators.py
@@ -1,5 +1,6 @@
 from django.shortcuts import redirect
 from groups.models import SportsGroup, Board
+from hs.models import MainBoard
 
 
 def is_member(view_func):
@@ -13,13 +14,23 @@ def is_member(view_func):
         return view_func(request, *args, **kwargs)
     return _wrapped_view_func
 
-
 def is_board(view_func):
     def _wrapped_view_func(request, *args, **kwargs):
         try:
             if request.user not in SportsGroup.objects.get(slug=kwargs['slug']).active_board:
                 return redirect('group_index', slug=kwargs['slug'])
         except Board.DoesNotExist:
+            pass
+
+        return view_func(request, *args, **kwargs)
+    return _wrapped_view_func
+
+def is_main_board(view_func):
+    def _wrapped_view_func(request, *args, **kwargs):
+        try:
+            if request.user not in MainBoard.objects.all().get():
+                return redirect('list_groups')
+        except MainBoard.DoesNotExist:
             pass
 
         return view_func(request, *args, **kwargs)
