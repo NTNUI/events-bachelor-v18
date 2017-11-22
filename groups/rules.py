5f2f7fa73dee1f377a109acf6a6a2f274bd8b326
MathiasM <Mathiamu@stud.ntnu.no>
2017-11-22 11:42:01 +0100

    Fix let all group board members invite

--- groups/rules.py
+++ groups/rules.py
@@ -48,7 +48,7 @@ add_perm('groups.can_see_members', is_group_board_member)
 add_perm('groups.can_see_settings', is_group_member)
 add_perm('groups.can_see_group_settings', is_group_leader | is_group_vp)
 add_perm('groups.can_see_invitations', is_group_board_member)
-add_perm('groups.can_invite_member', is_group_leader | is_group_vp)
+add_perm('groups.can_invite_member', is_group_board_member)
 add_perm('groups.can_leave_group', (~is_group_leader  # pylint:disable=invalid-unary-operand-type # noqa
                                     & ~is_group_vp  # pylint:disable=invalid-unary-operand-type # noqa
                                     & ~is_group_cashier)  # pylint:disable=invalid-unary-operand-type # noqa
