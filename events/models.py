14ba145922739b8902e60d004d32297af4b35297
Unknown <pciverse@stud.ntnu.no>
2018-02-07 15:13:26 +0100

    Added event model

--- events/models.py
+++ events/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from groups.models import SportsGroup
 
-# Create your models here.
+
+"""Creates a model for events, with start date, end date, priority, host group and ntnui as host.
+If ntnui is host, is_host_ntnui is true, otherwise false. 
+"""
+class Events(models.Model):
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    priority = models.BooleanField(default=False)
+    is_host_ntnui = models.BooleanField(default=False)
+    sports_group = models.ManyToManyField(SportsGroup)
+
+"""Add description and name to event, this way a event can have name and description in different languages.
+"""
+class EventDescription(models.Model):
+    name = models.CharField(max_length=100)
+    description_text = models.CharField(max_length=500)
+    language = models.CharField(max_length=30)
+    event = models.ForeignKey(Events)
+
