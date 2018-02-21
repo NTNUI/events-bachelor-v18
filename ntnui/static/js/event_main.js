d1272975bd6cee538c0976b04f00015cb804db0d
matssa <matssa@stud.ntnu.no>
2018-02-21 15:55:55 +0100

    up-to-dating for other users

--- ntnui/static/js/event_main.js
+++ ntnui/static/js/event_main.js
@@ -1,23 +1,21 @@
-const URL = '/ajax/events/get-events'
-let pageNr
-let pageCount
+const URL = "/ajax/events/get-events";
+let pageNr;
+let pageCount;
 
 // on document ready, send ajax request for the first page
-$( () => {
-    loadEvents(1)
-    $('#load-more').click( () => {
-        getNextPage()
-        }
-    )
-})
+$(() => {
+    loadEvents(1);
+    $("#load-more").click(() => {
+        getNextPage();
+    });
+});
 
 function getNextPage() {
-    if(pageNr < pageCount) {
-        pageNr = pageNr+1
-        loadEvents(pageNr)
-    }
-    else {
-        removeButton()
+    if (pageNr < pageCount) {
+        pageNr = pageNr + 1;
+        loadEvents(pageNr);
+    } else {
+        removeButton();
     }
 }
 
@@ -26,42 +24,38 @@ function loadEvents(page) {
     $.ajax({
         dataType: "json",
         url: URL,
-        data: {page: page},
-        success: (data) => {
-            console.log(data)
-            pageNr = parseInt(data.page_number)
-            pageCount = data.page_count
-            displayEvents(data.events)
+        data: { page: page },
+        success: data => {
+            console.log(data);
+            pageNr = parseInt(data.page_number);
+            pageCount = data.page_count;
+            displayEvents(data.events);
         },
-        error: (data) => {
-            displayError(data.message)
+        error: data => {
+            displayError(data.message);
         }
-    })
+    });
 }
 
 // for each event dispay event
 function displayEvents(events) {
-    events.map((event) => {
-        displayEvent(event)
-    })
+    events.map(event => {
+        displayEvent(event);
+    });
 }
 
 // display one event
 function displayEvent(event) {
-    $('#event-container').append(
-        "<div>" + event.name + "<div/>"
-    )
+    $("#event-container").append(
+        "<div>" + event.name + ": " + event.start_date + "<div/>"
+    );
 }
 
 // display error if something went wrong
 function displayError(msg) {
-    $('#event-container').html(
-        "<div>" + msg + "<div/>"
-    )
+    $("#event-container").html("<div>" + msg + "<div/>");
 }
 
 function removeButton() {
-    $('#load-more').hide()
-
+    $("#load-more").hide();
 }
-
