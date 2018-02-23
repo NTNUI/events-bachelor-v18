e16a488901d9b003e379a4b373053abd46987b06
oyvinabj <oyvinabj@stud.ntnu.no>
2018-02-23 16:18:47 +0100

    Validation largely complete, need to fix css

--- ntnui/static/js/create_new_event.js
+++ ntnui/static/js/create_new_event.js
@@ -54,10 +54,99 @@ function slideUpAlert(redirect) {
     //set timeout
     setTimeout( () => {
         //slide up the alert
-    $(".alert").slideUp(500)
+    $(".alert")
     if(redirect){
         window.location.href = '/events'
     }
     //sets the amount of ms before the alert is closed
-    }, 1500)
+    })
 }
+
+var name_no = document.getElementsByName("name_no")[0];
+var name_en = document.getElementsByName("name_en")[0];
+var start_date = document.getElementsByName("start_date")[0];
+var end_date = document.getElementsByName("end_date")[0];
+var description_text_no = document.getElementsByName("description_text_no")[0];
+var description_text_en = document.getElementsByName("description_text_en")[0];
+
+
+function validateName_no() {
+    if (!name_no.checkValidity()) {
+
+        document.getElementById("name_no_message").innerHTML = "Invalid input!";
+
+    }
+    else{
+        document.getElementById("name_no_message").innerHTML = "";
+    }
+}
+
+function validateName_en(){
+    if (!name_en.checkValidity()) {
+
+        document.getElementById("name_en_message").innerHTML = "Invalid input!";
+    }
+    else{
+        document.getElementById("name_en_message").innerHTML = "";
+    }
+    }
+
+function validateStart_date(){
+    var today = new Date().toISOString();
+
+    today = today.slice(0, -8);
+    var input_End_date = end_date.value.toLocaleString();
+    var input_Start_date = start_date.value.toLocaleString();
+    if (!(input_Start_date > today)){
+        start_date.setCustomValidity("Invalid date!");
+        document.getElementById("start_date_message").innerHTML = start_date.validationMessage;
+    }
+    else if(!(input_End_date=="")) {
+        if (!(input_End_date > input_Start_date)) {
+            start_date.setCustomValidity("Start date is not before end date!");
+            document.getElementById("start_date_message").innerHTML = start_date.validationMessage;
+        }
+    }
+    else{
+        document.getElementById("start_date_message").innerHTML = "";
+    }
+}
+
+function validateEnd_date(){
+    var today = new Date().toISOString();
+    today = today.slice(0, -8);
+    var input_Start_date = start_date.value.toLocaleString();
+    var input_End_date = end_date.value.toLocaleString();
+    if (!(input_Start_date > today)){
+        start_date.setCustomValidity("Invalid date!");
+        document.getElementById("end_date_message").innerHTML = start_date.validationMessage;
+    }
+    else if(!(input_End_date > input_Start_date)){
+        start_date.setCustomValidity("End date is not after start date!");
+        document.getElementById("end_date_message").innerHTML = start_date.validationMessage;
+    }
+    else{
+        document.getElementById("end_date_message").innerHTML = "";
+    }
+}
+
+function validateDescription_text_no() {
+    if (!description_text_no.checkValidity()) {
+        document.getElementById("description_no_message").innerHTML = "Empty field!";
+    }
+    else{
+        document.getElementById("description_no_message").innerHTML = "";
+
+    }
+}
+
+function validateDescription_text_en(){
+    if (!description_text_en.checkValidity()) {
+        document.getElementById("description_en_message").innerHTML = "Empty field";
+    }
+    else{
+        document.getElementById("description_en_message").innerHTML = "";
+
+    }
+}
+
