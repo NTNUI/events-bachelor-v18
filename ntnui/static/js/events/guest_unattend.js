84a406a00a435074b46d7c3a834e297de5714ac3
Paalch <pciverse@stud.ntnu.no>
2018-05-15 17:36:34 +0200

    Added test imgs

--- ntnui/static/js/events/guest_unattend.js
+++ ntnui/static/js/events/guest_unattend.js
@@ -12,7 +12,7 @@ $(() => {
         if (!$(button).prop('disabled')) {
             $(button).prop("disabled", true).addClass("disabled");
             setButtonLoader(button, '#AA0000')
-            response = await sendAjax({}, ("/events/remove-attendance/" + button.value), 'GET', button)
+            const response = await sendAjax({}, ("/events/remove-attendance/" + button.value), 'GET', button)
             if(response) {
                 $("#container").html("<i>" + gettext("You will now be redirected to the main event page") + "</i")
                 printMessage(MsgType.SUCCESS, response.message)
